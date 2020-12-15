fa12a0b6fa51d75855ffc475d2640746a2af420e
Tony Ciccarone <tonyvciccarone@gmail.com>
2020-12-15 12:40:27 -0500

    Front page and general styles & helpers

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Child Theme
+ *
+ */
+
+function fildisi_child_theme_setup() {
+
+}
+add_action( 'after_setup_theme', 'fildisi_child_theme_setup' );
+
+
+//Omit closing PHP tag to avoid accidental whitespace output errors.
+
+
+// WordPress function to automatically
+// add ALT tags to images on upload.
+// https://gist.github.com/ciccarone/db3985a34e95fff72118c01478b67088
+function alt_tag_adder( $post_ID ) {
+  if ( wp_attachment_is_image( $post_ID ) ) {
+    $tc_title = get_post( $post_ID )->post_title;
+    $tc_title = preg_replace( '%\s*[-_\s]+\s*%', ' ',  $tc_title );
+    $tc_title = ucwords( strtolower( $tc_title ) );
+    $tc_meta = array(
+      'ID'  =>  $post_ID,
+    );
+    update_post_meta( $post_ID, '_wp_attachment_image_alt', $tc_title );
+    wp_update_post( $tc_meta );
+  }
+}
+
+add_action( 'add_attachment', 'alt_tag_adder' );
+
+
+
+function custom_js() {
+  echo '<script src="https://kit.fontawesome.com/cff905c27f.js" crossorigin="anonymous"></script>';
+}
+add_action( 'wp_head', 'custom_js' );
